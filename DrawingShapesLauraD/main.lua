cd2cc4be6379f5a839725f2ce85d863befcd2872
s269431 <47784134+s269431@users.noreply.github.com>
2019-02-26 09:09:19 -0500

    Initial commit

---
+++ DrawingShapesLauraD/main.lua
@@ -0,0 +1,8 @@
+-- Title: DisplayingShapes
+-- Name: Duffley Duffley
+-- Course: ICS2O/3C
+-- This program displays four polygons.
+--The triangle has a base and a hight which is calculated.  
+
+--create my local variables
+local area
