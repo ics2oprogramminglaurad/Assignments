efc17b08c0e47f7fbebef9084471e2fca950edfe
s269431 <47784134+s269431@users.noreply.github.com>
2019-02-26 09:51:08 -0500

    Update main.lua

--- DrawingShapesLauraD/main.lua
+++ DrawingShapesLauraD/main.lua
@@ -2,7 +2,26 @@
 -- Name: Duffley Duffley
 -- Course: ICS2O/3C
 -- This program displays four polygons.
---The triangle has a base and a hight which is calculated.  
+--The triangle has a base and a hight which is calculated. FINSH THIS 
 
 --create my local variables
-local area
+local areaText
+local textSize = 40
+
+local baseOfTriangle = 3
+local heightOfTriangle = 10
+local areaOfTriangle = baseOfTriangle * heightOfTriangle / 2
+
+local pentagonVertices 
+local octagonVertices 
+local hexagon vertices  
+
+
+-- set the background color of the screen.
+display.setDefault("background" 252/255, 241/255, 172/255 )
+
+--to remove the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- draw the triangle
+display.newPolygon( 50,50 )
